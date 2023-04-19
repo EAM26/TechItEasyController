b351820b8ab7f29fc28c42ced8338cd91f6a8a06
EAM26 <emuller2607@gmail.com>
2023-04-19 19:55:54 +0200

    first commit

---
+++ src/main/java/com/example/sbtechiteasycontroller/SbTechItEasyControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbtechiteasycontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTechItEasyControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbTechItEasyControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbtechiteasycontroller/SbTechItEasyControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbtechiteasycontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTechItEasyControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
